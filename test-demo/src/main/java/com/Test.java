1ca0834074ac3993b548ddec84972f4c5030f3cd
umingking <umingking@aliyun.com>
2017-09-06 12:14:01 +0800

    增加一个类

---
+++ test-demo/src/main/java/com/Test.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ *
+ * @author jinyouming
+ * @email  jinyouming@tuandai.com
+ * @date   2017-9-6 下午1:45:10
+ * @Copyright Copyright (c) 2017 Niiwoo Inc. All Rights Reserved.
+ * @desc
+ */
+public class Test {
+
+}
+
