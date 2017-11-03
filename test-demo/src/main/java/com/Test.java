8f1ed0cc5c1f5742c86665a474db145fd892c65e
umingking <umingking@aliyun.com>
2017-11-03 18:02:41 +0800

    注释掉

--- test-demo/src/main/java/com/Test.java
+++ test-demo/src/main/java/com/Test.java
@@ -12,7 +12,7 @@ public class Test {
     
     public static void main(String[] args) {
         System.out.println("hello test world !!");
-        System.out.println("test");
+        //System.out.println("test");
     }
 
 }
